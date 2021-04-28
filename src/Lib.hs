3264a778097d58387ef797d8db810c608dac1f6e
Yuto Takano <moa17stock@gmail.com>
2021-04-28 22:35:11 +0100

    Take URL from cli arg, and make embed with pattern matching

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,26 +1,35 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module Lib
     ( main
     ) where
 
 import           Control.Monad.IO.Class     ( liftIO )
 import           Data.Aeson
-import           Data.Aeson.Types           ( parseEither )
+import           Data.Aeson.Types           ( parseEither
+                                            , parseMaybe
+                                            , Parser(..)
+                                            )
 import qualified Data.Text.Lazy as TL
+import qualified Data.Text as T
 import qualified Network.HTTP.Types.Status as Status
+import           System.Environment
 import           Web.Scotty
 
 import           GitHubWebhookDecoder
+import           DiscordSender
 
 -- | Entry point.
 main :: IO ()
-main = scotty 3000 $
-    post "/github" $ do
-        webhook <- decodeWebhook
-        liftIO $ putStrLn $ "Received webhook."
-        liftIO $ print webhook
-        html $ "Hello"
+main = do
+    args <- getArgs
+    webhookUrl <- case () of
+        _ | length args == 0 -> error "URL not supplied"
+        _                    -> pure $ head args
+    scotty 3000 $
+        post "/github" $ do
+            webhook <- decodeWebhook
+            liftIO $ sendEmbed webhookUrl $ createEmbed webhook
+            liftIO $ print webhook
+            html $ "Success."
 
 -- | Decode the JSON in the request body to a Webhook.
 -- Throws 400 if the body is empty or unparsable.
@@ -42,3 +51,50 @@ badReq t = do
     status Status.badRequest400
     text t
     finish
+
+createEmbed :: Webhook -> Embed
+createEmbed (Webhook c a (Project v)) = createProjectEmbed c a v
+createEmbed (Webhook c a (ProjectColumn v)) = createProjectColumnEmbed c a v
+createEmbed (Webhook c a (ProjectCard v)) = createProjectCardEmbed c a v
+
+createProjectEmbed :: Context -> Action -> Value -> Embed
+createProjectEmbed c a v = Embed { embedTitle = title
+                                 , embedType = "rich"
+                                 , embedDescription = ""
+                                 , embedUrl = projectBoardUrl
+                                 , embedColor = colorFromAction a
+                                 }
+  where
+    title :: T.Text
+    title = titlePrefix <> "Project Board " <> (T.pack $ show a) <> ": " <> projectBoardName
+
+    titlePrefix :: T.Text
+    titlePrefix = "[" <> repoOwnerName c <> "/" <> repoName c <> "] "
+
+    projectBoardName :: T.Text
+    projectBoardName = parseMaybeDef parseBoardName "" v
+
+    projectBoardUrl :: T.Text
+    projectBoardUrl = parseMaybeDef parseBoardUrl "" v
+
+createProjectColumnEmbed :: Context -> Action -> Value -> Embed
+createProjectColumnEmbed = undefined
+
+createProjectCardEmbed :: Context -> Action -> Value -> Embed
+createProjectCardEmbed = undefined
+
+parseMaybeDef :: (a -> Parser b) -> b -> a -> b
+parseMaybeDef parser def v  =
+    case parseMaybe parser v of
+        Nothing -> def
+        Just x  -> x
+
+colorFromAction :: Action -> Integer
+colorFromAction Created = 6667344 -- #65bc50
+colorFromAction Edited = 16766784 -- #ffd740
+colorFromAction Moved = 0
+colorFromAction Converted = 16766784 -- #ffd740
+colorFromAction Closed = 0
+colorFromAction Reopened = 11771355 -- #b39ddb
+colorFromAction Deleted = 6588634 -- #6488da
+ 
